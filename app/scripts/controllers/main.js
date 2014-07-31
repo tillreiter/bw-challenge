b259d9603fb39896e35a65e85bd8a81c00789579
Till Reiter <till.reiter@gmail.com>
2014-07-31 17:02:47 +0200

    implement showing metadata on ng-click

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,24 +3,34 @@
 angular.module('bwChallengeApp')
   .controller('MainCtrl', function ($scope, Topics) {
 
+
     $scope.dataLoaded = false;
+    $scope.currentTopic = {
+      label: "/ ",
+      volume: "/ ",
+      sentiment: {
+        positive: "/ ",
+        neutral: "/ ",
+        negative: "/ "
+      }
+    };
 
     $scope.setStyle = function (topic) {
-
-      var popularityTextSize = 16 + (topic.relativeTextSize * 5);
-
+      var popularityTextSize = 12 + (topic.relativeTextSize * 5);
       return {'color': topic.sentimentTextColor, 'font-size': popularityTextSize, 'text-align': topic.randomAlignment};
     };
 
-    $scope.topics = "Data loading";
+    $scope.showInfo = function (topic) {
+      $scope.currentTopic = topic;
+    };
 
     Topics.getJSON().success(function (data) {
       $scope.topics = data.topics;
       console.log('loading data finished');
       $scope.preparedTopics = Topics.prepare($scope.topics);
+      console.log('preparing data finished');
       $scope.dataLoaded = true;
     });
-
 
   });
 
