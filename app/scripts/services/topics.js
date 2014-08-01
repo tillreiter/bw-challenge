eb80b463810c768bb920f6a7ce0f72aa7ca14c7b
Till Reiter <till.reiter@gmail.com>
2014-08-01 18:08:50 +0100

    comment topics service

--- app/scripts/services/topics.js
+++ app/scripts/services/topics.js
@@ -4,73 +4,82 @@ angular.module('bwChallengeApp')
 
   .factory('Topics', ['$http', function ($http) {
 
-  function setTextSize(volume, classSizePopularity) {
-    return (12 + (Math.floor(volume / classSizePopularity) * 5))
-  }
-
-  function setRandomAlignment() {
-    var alignment = ['left', 'center', 'right'];
-    var num = (Math.floor(Math.random() * 3));
-    return alignment[num];
-  }
-
-  function setRandomColumnSize() {
-    var column = [3, 4];
-    var num = (Math.floor(Math.random() * 2));
-    return column[num];
-  }
-
-  function setSentimentTextColor(topic) {
-    var fontColor = null;
-    if (topic.sentimentScore < 40) {
-      fontColor = "#ff0124"; //red
-    } else if (topic.sentimentScore > 60) {
-      fontColor = "#33be2a";  //green
-    } else {
-      fontColor = "#696969"; //grey
+    function setTextSize(volume, classSizePopularity) {
+      var textSize = 12;
+      var extraPopularitySize = Math.floor(volume / classSizePopularity) * 5;
+      return (textSize + extraPopularitySize);
     }
-    return fontColor
-  }
 
-  var methods = {
+    function setRandomAlignment() {
+      var alignments = ['left', 'center', 'right'];
+      var num = (Math.floor(Math.random() * 3));
+      return alignments[num];
+    }
 
-    getJSON: function (path) {
-      return $http.get(path);
-    },
+    function setRandomColumnSize() {
+      var comlumnSize = [3, 4];
+      var num = (Math.floor(Math.random() * 2));
+      return comlumnSize[num];
+    }
 
-    prepare: function (input) {
-      var amountTopics = input.length,
-        arr = [];
-
-//    find most/least popular topic and calculate range for text sizes
-      var mostPopular = _.max(input, function (topics) {
-        return topics.volume;
-      });
-
-      var leastPopular = _.min(input, function (topics) {
-        return topics.volume;
-      });
-
-      var classSizePopularity = (mostPopular.volume - leastPopular.volume) / 6;
-
-
-//    filter out keys for further processing
-      for (var i = 0; i < amountTopics; i++) {
-        var obj = {};
-        obj = _.pick(input[i], 'label', 'volume', 'sentiment', 'sentimentScore');
-        obj.popularityTextSize = setTextSize(input[i].volume, classSizePopularity);
-        obj.randomAlignment = setRandomAlignment();
-        obj.randomColumnSize = setRandomColumnSize();
-        obj.sentimentTextColor = setSentimentTextColor(input[i]);
-
-        arr.push(obj);
+    function setSentimentTextColor(topic) {
+      var fontColor = null;
+      if (topic.sentimentScore < 40) {
+        fontColor = "#ff0124"; //red
+      } else if (topic.sentimentScore > 60) {
+        fontColor = "#33be2a";  //green
+      } else {
+        fontColor = "#696969"; //grey
       }
-
-//    shuffle data for random positioning (biggest in the middle would also make sense - example looked shuffled)
-      return _.shuffle(arr);
+      return fontColor
     }
-  };
 
-  return methods;
+    var methods = {
+
+      //http service to get JSON file, deferred success is accessed via main controller
+      getJSON: function (path) {
+        return $http.get(path);
+      },
+
+      //clean data of unneeded information and add styling information
+      prepare: function (input) {
+        var amountTopics = input.length,
+          arr = [];
+
+        //find most/least popular topic according to volume label
+        var mostPopular = _.max(input, function (topics) {
+          return topics.volume;
+        });
+
+        var leastPopular = _.min(input, function (topics) {
+          return topics.volume;
+        });
+
+        //calculate range for sizing classes
+        var classSizePopularity = (mostPopular.volume - leastPopular.volume) / 6;
+
+        //read every topic
+        for (var i = 0; i < amountTopics; i++) {
+          var obj = {};
+
+          //create new object only with key values for further processing
+          obj = _.pick(input[i], 'label', 'volume', 'sentiment', 'sentimentScore');
+
+          //add new values for viewing
+          obj.popularityTextSize = setTextSize(input[i].volume, classSizePopularity);
+          obj.randomAlignment = setRandomAlignment();
+          obj.randomColumnSize = setRandomColumnSize();
+          obj.sentimentTextColor = setSentimentTextColor(input[i]);
+
+          //push to array that gets send back
+          arr.push(obj);
+        }
+
+        //shuffle data for random positioning (biggest in the middle would also make sense - example looked shuffled)
+        return _.shuffle(arr);
+      }
+    };
+
+    return methods;
 
   }]);
