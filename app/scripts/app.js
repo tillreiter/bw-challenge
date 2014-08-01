58d01b825b16288d908b5b49c5253b9bf85b349f
Till Reiter <till.reiter@gmail.com>
2014-08-01 16:14:06 +0100

    include http loading bar and remove ngRoute

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module('bwChallengeApp', ['ngRoute']);
+angular.module('bwChallengeApp', ['angular-loading-bar'])
+
+  //include a loading bar for http request
+  .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.latencyThreshold = 10;
+  }])
